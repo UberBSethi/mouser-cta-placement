a6c3913bcaf85810fd47ca50e9691d188fb0f971
UberBSethi <bharat.sethi@uberflip.com>
2021-11-19 14:56:21 -0500

    CTA Placement App

--- scripts/app.js
+++ scripts/app.js
@@ -1,61 +1,18 @@
-window.updatedABMstreams2020 = window.updatedABMstreams2020 || [];
-window.streamInfo2020 = window.streamInfo2020 || {};       
-const on = {{stream_app_on}};
-window.streamAppOn = on;
-const stream_id = Hubs.appInstance.currentCollectionId;
-if(on === true) {
-  window.updatedABMstreams2020 = [stream_id];
-  const csc = [
-    {streamId: {{csc_stream_a}}, tabTitle: '{{csc_tab_title_a}}'},
-    {streamId: {{csc_stream_b}}, tabTitle: '{{csc_tab_title_b}}'},
-    {streamId: {{csc_stream_c}}, tabTitle: '{{csc_tab_title_c}}'},
-    {streamId: {{csc_stream_d}}, tabTitle: '{{csc_tab_title_d}}'},
-    {streamId: {{csc_stream_e}}, tabTitle: '{{csc_tab_title_e}}'}
+{% if mouser_app_on == "true" %}
+<script>
+  const streamId = Hubs.appInstance.currentCollectionId.toString();
+  window.obData.ctasToManipulate = [];
+  const ctaList = [
+  {streamId: streamId, ctaId: '{{ mouser_cta_id_a }}', newPos: {{ mouser_pos_a }}},
+  {streamId: streamId, ctaId: '{{ mouser_cta_id_b }}', newPos: {{ mouser_pos_b }}},
+  {streamId: streamId, ctaId: '{{ mouser_cta_id_c }}', newPos: {{ mouser_pos_c }}},
+  {streamId: streamId, ctaId: '{{ mouser_cta_id_d }}', newPos: {{ mouser_pos_d }}},
+  {streamId: streamId, ctaId: '{{ mouser_cta_id_e }}', newPos: {{ mouser_pos_e }}}
   ];
-    
-  window.streamInfo2020 = {
-    [stream_id]: {
-    templateType: "{{template_type}}", 
-    industry: "{{industry}}", 
-    topHeading: "{{top_heading}}", 
-    bottomHeading: "{{bottom_heading}}", 
-    subHeading: "{{sub_heading}}.", 
-    salesRepId: "{{sales_rep_id}}", 
-    showDataBlocks: {{show_data_blocks}}, 
-    eventsHeadline: "{{events_headline}}", 
-    companySpecificContent: [],
-    companyContentTileAmount: {{company_content_tile_amount}}, 
-    midPageAdPlacement: {{mid_page_ad_placement}},
-    quoteSectionTitle: "{{quote_section_title}}"
-
-    }
-  };
-  let note = "{{s_rep_pm}}";
-  
-  if(note !== 'empty'){
-    window.salesRepInfo["{{sales_rep_id}}"].repPersonalNote[stream_id] = note;
-  }
-  csc.forEach((item) => {
-    if(item.streamId !== 'empty' && typeof item.streamId === "number") {
-      window.streamInfo2020[stream_id].companySpecificContent.push(item);
+  ctaList.forEach((item) => {
+    if(item.ctaId !== 'empty' && typeof item.ctaId === "string") {
+    window.obData.ctasToManipulate.push(item);
     }
   });
-};
-
-
-
-
-"Advertising, Media & Entertainment"
-"Energy"
-"Federal"
-"Financial Services"
-"Food"
-"Healthcare"
-"Hotel"
-"Life Sciences"
-"Manufacturing"
-"Media"
-"Retail & CPG"
-"State and Local Government"
-"Technology"
-"Warehouse"
+</script>
+{% endif %}
