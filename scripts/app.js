d04959dac742e2ab48cf55743aa4bbcd548c2814
UberBSethi <bharat.sethi@uberflip.com>
2021-11-22 12:20:59 -0500

    added default

--- scripts/app.js
+++ scripts/app.js
@@ -3,11 +3,11 @@
   const streamId = Hubs.appInstance.currentCollectionId.toString();
   window.obData.ctasToManipulate = [];
   const ctaList = [
-  {streamId: streamId, ctaId: '{{ mouser_cta_id_a }}', newPos: {{ mouser_pos_a }}},
-  {streamId: streamId, ctaId: '{{ mouser_cta_id_b }}', newPos: {{ mouser_pos_b }}},
-  {streamId: streamId, ctaId: '{{ mouser_cta_id_c }}', newPos: {{ mouser_pos_c }}},
-  {streamId: streamId, ctaId: '{{ mouser_cta_id_d }}', newPos: {{ mouser_pos_d }}},
-  {streamId: streamId, ctaId: '{{ mouser_cta_id_e }}', newPos: {{ mouser_pos_e }}}
+  {streamId: streamId, ctaId: '{{ mouser_cta_id_a }}', newPos: {{ mouser_pos_a | default: 0 }}},
+  {streamId: streamId, ctaId: '{{ mouser_cta_id_b }}', newPos: {{ mouser_pos_b | default: 0 }}},
+  {streamId: streamId, ctaId: '{{ mouser_cta_id_c }}', newPos: {{ mouser_pos_c | default: 0 }}},
+  {streamId: streamId, ctaId: '{{ mouser_cta_id_d }}', newPos: {{ mouser_pos_d | default: 0 }}},
+  {streamId: streamId, ctaId: '{{ mouser_cta_id_e }}', newPos: {{ mouser_pos_e | default: 0 }}}
   ];
   ctaList.forEach((item) => {
     if(item.ctaId !== 'empty' && typeof item.ctaId === "string") {
